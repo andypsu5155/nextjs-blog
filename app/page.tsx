d6104044d4c4373f75eec75bd0a327befdbedae0
Andy Albaugh <andypsu5155@gmail.com>
2023-12-31 03:13:36 -0500

    About to deploy!

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "@/lib/interface";
 import { client, urlFor } from "@/lib/sanity";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getData() {
   const query = `
@@ -22,10 +24,23 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
       {data.map((post, idx) => (
         <Card key={idx}>
-          <Image src={urlFor(post.titleImage).url()} alt="image" width={500} height={500} />
+          <Image 
+            src={urlFor(post.titleImage).url()} 
+            alt="image" 
+            width={500} 
+            height={500} 
+            className="rounded-t-lg h-[200px] object-cover"
+          />
+          <CardContent>
+            <h3 className="text-lg line-clamp-2">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-400">{post.smallDescription}</p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`blog/${post.currentSlug}`} className="text-lg line-clamp-2 font-bold">Read More</Link>
+            </Button>
+          </CardContent>
         </Card>
       ))}
     </div>
