bf78099ea6f90f924cab0695315a0a135ae5d49c
Andy Albaugh <andypsu5155@gmail.com>
2023-12-31 02:33:22 -0500

    Finished basic sanity integration 55:56

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Navbar from "@/components/navbar";
+import { Card } from "@/components/ui/card";
+import { simpleBlogCard } from "@/lib/interface";
+import { client, urlFor } from "@/lib/sanity";
+import Image from "next/image";
 
-export default function Home() {
+async function getData() {
+  const query = `
+    *[_type == 'blog'] | order(_createdAt desc) {
+      title,
+        smallDescription,
+        "currentSlug": slug.current,
+        titleImage,
+    }
+  `;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
+
   return (
-    <div>
-      <h1>Hello from the index page</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image src={urlFor(post.titleImage).url()} alt="image" width={500} height={500} />
+        </Card>
+      ))}
     </div>
   )
 }
